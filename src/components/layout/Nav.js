172386c096e314a1f74ead3433978252a0daa331
SanderOlsen98 <sanderlolsen@gmail.com>
2022-09-27 14:45:20 +0200

    update

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,12 +1,30 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useHistory } from "react-router-dom";
+import AuthContext from "../../context/AuthContext";
 
 function Nav() {
-	return (
-		<nav>
-			<Link to="/">Home</Link>
-			<Link to="/login">Login</Link>
-		</nav>
-	);
+  const [auth, setAuth] = useContext(AuthContext);
+
+  const history = useHistory();
+
+  function logout() {
+    setAuth(null);
+    history.push("/");
+  }
+
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      {auth ? (
+        <>
+          | <Link to="/dashboard">Dashboard</Link> |{" "}
+          <button onClick={logout}>Log out</button>
+        </>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+    </nav>
+  );
 }
 
 export default Nav;
